a535b10b9507a62359134af9f9e16e87debec91e
Alexei Voloshyn <75684845+aleksvoloshyn@users.noreply.github.com>
2023-02-20 11:35:35 +0200

    cv files added

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import Container from '../Container/Container'
+import Cv from '../Cv/Cv'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Cv />
+    </div>
+  )
+}
+
+export default App
